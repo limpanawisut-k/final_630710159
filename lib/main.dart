334c00308668548e60227a02c52b1d6b129557eb
Kitsanarat Limpanawisut <limpanawisut_k@silpakorn.edu>
2023-10-26 01:30:20 +0700

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Homework_3',
+      title: 'final_630710159',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
